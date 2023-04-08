09365621671410e23988b38c9a554aa72cf53380
Jellevanpol <118268871+Jellevanpol@users.noreply.github.com>
2023-04-08 19:25:31 +0200

    1st push

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
